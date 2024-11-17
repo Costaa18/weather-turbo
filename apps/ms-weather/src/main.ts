12137a4f44ed3631ea5960e6a7bf3e133d3efdaa
costa <afonsovelosof@gmail.com>
2024-11-17 21:09:25 +0000

    feat(ms-weather): enhance microservice bootstrap with environment variables and graceful shutdown handling

--- apps/ms-weather/src/main.ts
+++ apps/ms-weather/src/main.ts
@@ -1,17 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { WeatherModule } from './weather.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); 
 
 async function bootstrap() {
-  const app = await NestFactory.create(WeatherModule);
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.REDIS,
-    options: {
-      port: 6379,
-      host: 'redis',
-    },
+  const logger = new Logger('Bootstrap');
+
+  try {
+    const app = await NestFactory.create(WeatherModule);
+
+    app.connectMicroservice<MicroserviceOptions>({
+      transport: Transport.REDIS,
+      options: {
+        name: 'WEATHER_SERVICE',
+        host: process.env.REDIS_HOST || 'redis',
+        port: parseInt(process.env.REDIS_PORT, 10) || 6379,
+      },
+    });
+
+    await app.startAllMicroservices();
+    logger.log('Microservice WEATHER_SERVICE is running and connected to Redis.');
+  } catch (error) {
+    logger.error('Failed to start the microservice', error.stack);
+    process.exit(1); // Encerra o processo em caso de erro fatal
+  }
+
+  // Manipulação de sinais para encerramento controlado
+  process.on('SIGINT', async () => {
+    logger.log('SIGINT received. Shutting down microservice gracefully.');
+    process.exit(0);
   });
-  await app.startAllMicroservices();
-  await app.listen(4000);
+
+  process.on('SIGTERM', async () => {
+    logger.log('SIGTERM received. Shutting down microservice gracefully.');
+    process.exit(0);
+  });
 }
 bootstrap();
