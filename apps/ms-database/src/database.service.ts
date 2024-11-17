07ad251890935d4b20d8b9b1fdda1026236caae3
costa <afonsovelosof@gmail.com>
2024-11-17 21:11:51 +0000

    feat(ms-database): add DatabaseService for handling weather data storage

---
+++ apps/ms-database/src/database.service.ts
@@ -0,0 +1,17 @@
+// database.service.ts
+import { Injectable } from '@nestjs/common';
+import { SupabaseService } from './supabase/subapase.service';
+
+@Injectable()
+export class DatabaseService {
+  constructor(private readonly supabaseService: SupabaseService) { }
+
+  async saveWeatherData(ip: string, weatherData: WeatherData, city?: string): Promise<void> {
+    if (!weatherData || typeof weatherData !== 'object') {
+      throw new Error('Invalid weather data');
+    }
+
+    // Save weather data using the Supabase service
+    await this.supabaseService.saveWeatherData(ip, weatherData, city);
+  }
+}
