a7d16ddf090907f7f7904066fc939b44781c1cfb
costa <afonsovelosof@gmail.com>
2024-11-17 21:09:10 +0000

    feat(ms-geo): update main entry point to use GeoModule and configure Redis microservice

--- apps/ms-geo/src/main.ts
+++ apps/ms-geo/src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { GeoModule } from './geo.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('Bootstrap');
+
+  try {
+    const app = await NestFactory.create(GeoModule);
+
+    app.connectMicroservice<MicroserviceOptions>({
+      transport: Transport.REDIS,
+      options: {
+        name: 'GEO_SERVICE',
+        host: process.env.REDIS_HOST || 'redis',
+        port: parseInt(process.env.REDIS_PORT, 10) || 6379,
+      },
+    });
+
+    await app.startAllMicroservices();
+    logger.log('Microservice GEO_SERVICE is running and connected to Redis.');
+  } catch (error) {
+    logger.error('Failed to start the microservice', error.stack);
+    process.exit(1); // Encerra o processo em caso de erro fatal
+  }
+
+  // Manipulação de sinais para encerramento controlado
+  process.on('SIGINT', async () => {
+    logger.log('SIGINT received. Shutting down microservice gracefully.');
+    process.exit(0);
+  });
+
+  process.on('SIGTERM', async () => {
+    logger.log('SIGTERM received. Shutting down microservice gracefully.');
+    process.exit(0);
+  });
 }
 bootstrap();
