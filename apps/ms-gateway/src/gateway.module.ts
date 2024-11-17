ed08c8baf72d98f62630393cf34cefc4434797f5
costa <afonsovelosof@gmail.com>
2024-11-17 23:34:37 +0000

    feat(ms-gateway): remove GatewayController and GatewayService from the module

--- apps/ms-gateway/src/gateway.module.ts
+++ apps/ms-gateway/src/gateway.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { WeatherModule } from './weather/weather.module';
-import { GatewayController } from './gateway.controller';
-import { GatewayService } from './gateway.service';
+
 import { MicroservicesModule } from './microservices/microservices.module';
 
 @Module({
   imports: [WeatherModule, MicroservicesModule],
-  controllers: [GatewayController],
-  providers: [GatewayService],
 })
 export class GatewayModule { }
