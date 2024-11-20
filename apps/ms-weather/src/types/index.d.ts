5857e275de4154f2a098de08e7ee3c810b6671d0
costa <afonsovelosof@gmail.com>
2024-11-20 14:25:15 +0000

    feat(ms-weather): add current_time and visibility properties to WeatherData interface

--- apps/ms-weather/src/types/index.d.ts
+++ apps/ms-weather/src/types/index.d.ts
@@ -1,27 +1,28 @@
 export interface GeoData {
-    city: string;
-    country: string;
-  }
-  
-  export interface WeatherByCityDto {
-    city: string;
-  }
-  
-  export interface WeatherData {
-    temperature: number;
-    feelsLike: number;
-    tempMin: number;
-    tempMax: number;
-    pressure: number;
-    humidity: number;
-    windSpeed: number;
-    windDeg: number;
-    weather: string;
-    description: string;
-    city: string;
-    country: string;
-    sunrise: number;
-    sunset: number;
-    icon: string;
-  }
-  
+  city: string;
+  country: string;
+}
+
+export interface WeatherByCityDto {
+  city: string;
+}
+
+export interface WeatherData {
+  temperature: number;
+  feelsLike: number;
+  tempMin: number;
+  tempMax: number;
+  pressure: number;
+  humidity: number;
+  windSpeed: number;
+  windDeg: number;
+  weather: string;
+  description: string;
+  city: string;
+  country: string;
+  sunrise: number;
+  sunset: number;
+  icon: string;
+  current_time: number;
+  visibility: number;
+}
